9ddaeb7d9d3483d0f391148d638bca82f9ce2862
CapinCole89 <91431059+CapinCole89@users.noreply.github.com>
2022-11-18 21:08:57 -0500

    Test 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-zinc-500 to-zinc-800">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-gray-400 sm:text-[5rem] select-none">
-            N0zdive <span className="text-red-600">.</span> com
+            N0zdive <span className="text-blue-600">.</span> com
           </h1>
         </div>
       </main>
